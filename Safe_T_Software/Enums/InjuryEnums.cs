c04918124211a43cdc763c29076a246d1de6e732
Dakotah Hitze <dakotahhitze@gmail.com>
2021-04-02 13:21:36 -0700

    Created Enum holding value of all possible injury locations

---
+++ Safe_T_Software/Enums/InjuryEnums.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safe_T_Software.Enums
+{
+
+    /// <summary>
+    /// Holds the location that injuries could be located at. List could be updated with new locations
+    /// when they arise
+    /// </summary>
+    public enum InjuryEnums
+    {
+        Eye,
+        LeftArm,
+        RightArm,
+        LeftLeg,
+        RightLeg,
+        Fingers,
+        Toes,
+        Mouth,
+        Head,
+        Groin,
+        Chest,
+        Back,
+        LeftShoulder,
+        RightShoulder,
+        LeftKnee,
+        RightKnee,
+        LeftFoot,
+        RightFoot,
+        LeftHand,
+        RightHand,
+        Neck
+    }
+}
