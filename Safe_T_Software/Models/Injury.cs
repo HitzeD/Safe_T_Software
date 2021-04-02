7958a02d63cf84791c0bbb659c53b0ca158ed08f
Dakotah Hitze <dakotahhitze@gmail.com>
2021-04-02 13:22:40 -0700

    Basic Injury data type created to be an instance of an injury and hold all pertinent information

--- Safe_T_Software/Models/Injury.cs
+++ Safe_T_Software/Models/Injury.cs
@@ -1,6 +1,35 @@
-﻿namespace Safe_T_Software.Models
+﻿using System;
+using Safe_T_Software.Enums;
+
+namespace Safe_T_Software.Models
 {
-    internal class Injury
+    public class Injury
     {
+        private long InjuryID;
+        private InjuryEnums injury;
+        public string Description { get; set; }
+        private DateTime dateOfInjury;
+        private Employee employee;
+
+        public Injury(long id, InjuryEnums typeOfInjury, string descript, DateTime date, Employee emp)
+        {
+            InjuryID = id;
+            injury = typeOfInjury;
+            Description = descript;
+            dateOfInjury = date;
+            employee = emp;
+        }
+
+        public InjuryEnums GetInjury()
+        {
+            return this.injury;
+        }
+
+        public override string ToString()
+        {
+            return $"{employee.EmpLastName}, {employee.EmpLastName} had an injury located on his/her {injury} on {dateOfInjury}. {employee.GetInjuryCount()} total injuries.";
+        }
+
+
     }
 }
