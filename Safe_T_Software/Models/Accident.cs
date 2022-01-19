70f5a5e67d066b49babb1e9a66c9b85663810859
Dakotah Hitze <d-hitze@comcast.net>
2022-01-19 12:13:44 -0500

    Basic Classes for incidents

---
+++ Safe_T_Software/Models/Accident.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safe_T_Software.Models
+{
+    /// <summary>
+    ///     Accidents are incidents that result in losses (monetary or personnel)
+    /// </summary>
+    public class Accident : Incident
+    {
+
+    }
+}
