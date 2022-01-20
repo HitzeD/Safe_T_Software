5977c8c065ef7a5379511b4851d04c74f93a1f55
Dakotah Hitze <d-hitze@comcast.net>
2022-01-20 08:48:00 -0500

    base framework for duty titles

--- Safe_T_Software/Models/DutyTitle.cs
+++ Safe_T_Software/Models/DutyTitle.cs
@@ -1,6 +1,53 @@
-﻿namespace Safe_T_Software.Models
+﻿using System;
+
+namespace Safe_T_Software.Models
 {
-    internal class DutyTitle
+    public class DutyTitle
     {
+    //  Fields
+
+        private static Guid TitleID { get; set; }
+        private static string Title { get; set; }
+        private static string Description { get; set; }
+
+    // Constructors
+        
+        /// <summary>
+        ///     Basic constructor that only takes in a string for the title
+        /// </summary>
+        /// <param name="title">string value that contains the value of the duty title being inputted/assigned</param>
+        public DutyTitle(string title)
+        {
+            TitleID = Guid.NewGuid();
+            Title = title;
+        }
+
+        /// <summary>
+        ///     More in-depth constructor that allows for the description to be set
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="descript"></param>
+        public DutyTitle(string title, string descript)
+        {
+            TitleID = Guid.NewGuid();
+            Title = title;
+            Description = descript;
+        }
+
+    // Methods
+
+        public static string SetTitle(string newTitle)
+        {
+            Title = newTitle;
+            return Title;
+        }
+
+        public static string SetDecript(string descript)
+        {
+            Description = descript;
+            return Description;
+        }
+
+
     }
 }
